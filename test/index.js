a6ea2cf2bc5b24758f52ccb9e5c1edd33e5311af
Yueming Fang <zergdo@gmail.com>
2019-03-17 16:10:24 +0800

    jsdoc

--- test/index.js
+++ test/index.js
@@ -1,22 +1,24 @@
-const querystring = `_tables=accounts&_tables=user\
-&_fields[]=id&_fields[]=name&_fields[]=mail\
-&_values[]=ID&_values[]=NAME&_values[]=MAIL\
+const querystring = `_table=accounts&_table=user\
+&_column=id&_column=name&_column=mail\
+&_value=ID&_value=NAME&_value=MAIL\
 &spawned_gte=20190101&spawned_lte=20190105\
-&name_ne[]=admin&name_ne[]=root\
-&destroied_eq[]=true&destroied_eq[]=false\
+&name_ne=admin&name_ne=root\
+&destroied_eq=true&destroied_eq=false\
 &mail_end=%40mail.com\
-&_logic[]=and&_logic[]=and&_logic[]=or\
-&_asc=mail&_desc[]=spawned&_desc[]=modified\
+&_logic=and&_logic=and&_logic=or\
+&_asc=mail&_desc=spawned&_desc=modified\
 &_limit=20&_page=3`
 
 const qs = require('qs')
 const query = qs.parse(querystring)
+const LeoSQL = require('../src')
+const leosql = new LeoSQL(query)
 
 /**
 query = {
-  "_sets": "accounts",                   // 表（字符串或数组）
-  "_fields": ["id", "name", "mail"],     // 字段（字符串或数组）
-  "_values": ["ID", "NAME", "MAIL"],     // 值（字符串或数组）
+  "_table": "accounts",                  // 表（字符串或数组）
+  "_column": ["id", "name", "mail"],     // 字段（字符串或数组）
+  "_value": ["ID", "NAME", "MAIL"],      // 值（字符串或数组）
   "spawned_gte": "20190101",             // 大于等于（字符串）
   "spawned_lte": "20190105",             // 小于等于（字符串）
   "name_ne": ["admin", "root"],          // 不等于（字符串或数组）
@@ -30,12 +32,9 @@ query = {
 }
 */
 
-const LeoSQL = require('../src')
-const leosql = new LeoSQL(query)
-
-console.log(leosql.getTables())
-console.log(leosql.getFields())
-console.log(leosql.getValues())
+console.log(leosql.getTable())
+console.log(leosql.getColumn())
+console.log(leosql.getValue())
 console.log(leosql.getWhere())
 console.log(leosql.getLogic())
 console.log(leosql.getLimit())
